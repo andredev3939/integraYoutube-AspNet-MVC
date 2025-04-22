1152d3c88fb03964e9343e56186c517a3cddca0f
André, Desenvolvedor de Software <felipecarvalhoacdm@gmail.com>
2025-04-22 13:37:16 -0300

    Dados da requisição

--- ApiYtb/Controllers/HomeController.cs
+++ ApiYtb/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 using System.Diagnostics;
+using ApiYtb.Models;
+using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiYtb.Controllers
@@ -10,5 +13,23 @@ namespace ApiYtb.Controllers
         {
             return View();
         }
+        // Requisições assíncronas retornam "tasks". No caso, do tipo lista e seguindo o modelo escolhido 
+        private async Task<List<DetalhesVd>> BuscaVideos()
+        {
+            // Acessa o serviço da API de acordo com os dados fornecidos
+            var youtubeService = new YouTubeService(new BaseClientService.Initializer
+            {
+                ApiKey = "AIzaSyC8_JlbKd3T-ZtWIaS1mpAkmmQ49Ukt-dY",
+                ApplicationName = "YoutubeApiVideo"
+            });
+            // Tipos de dados a serem retornados, id do canal, ordenar por ordem de lançamento e quantia máxima de resultados
+            var searchRequest = youtubeService.Search.List("snippet, statistics");
+            searchRequest.ChannelId = "UCib793mnUOhWymCh2VJKplQ";
+            searchRequest.Order = SearchResource.ListRequest.OrderEnum.Date;
+            searchRequest.MaxResults = 25;
+
+            var searchResponse = await searchRequest.ExecuteAsync();
+        }
+        
     }
 }
---
+++ ApiYtb/Models/DetalhesVd.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiYtb.Models
+{
+    /* Propriedades buscadas da API externa */
+    public class DetalhesVd
+    { 
+        // "?" permite que o valor recebido seja nulo. Get e set são o equivalente aos getters e setters no Java
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Link { get; set; }
+        public string? ThumbnailUrl { get; set; }
+        public DateTime? PublishedAt { get; set; }
+    }
+}
